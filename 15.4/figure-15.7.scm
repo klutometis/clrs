d7ab77a1a53b78256d4e9c2d7f911d4f67b6729c
Peter Danenberg <pcd@wikitex.org>
2008-09-15 04:49:41 -0700

    bst not working

---
+++ 15.4/figure-15.7.scm
@@ -0,0 +1,7 @@
+(require-extension syntax-case)
+(require '../15.5/section)
+(import section-15.5)
+(let ((p (array-align (list->array 1 '(#f 0.15 0.10 0.05 0.10 0.20)) -1))
+      (q (array-align (list->array 1 '(0.05 0.10 0.05 0.05 0.05 0.10)) -1)))
+  (let-values (((e root) (optimal-bst p q (- (car (array-dimensions p)) 1 1))))
+    (pretty-print (array->list e))))
--- 15.5/section.scm
+++ 15.5/section.scm
@@ -1,13 +1,16 @@
 (require-extension
  syntax-case
  array-lib
+ foof-loop
  (srfi 31))
 (require '../10.1/section)
 (require '../12.1/section)
 (module
  section-15.5
  (construct-optimal-bst
-  make-optimal-bst)
+  make-optimal-bst
+  memoized-bst
+  optimal-bst)
  (import* section-10.1
           make-stack
           pop!
