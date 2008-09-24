71625baa1654e5c1eb1da71ae3bf262e19a9360e
Peter Danenberg <pcd@wikitex.org>
2008-09-23 19:16:36 -0700

    explanation

--- 2.3/2.3-7.scm
+++ 2.3/2.3-7.scm
@@ -6,3 +6,14 @@
     (check x => 13)
     (check y => 117))
   (check (sum-search S 129) => #f))
+
+;;; Returns either i) ordered pair (x, y) that adds to x, or ii) false
+;;; if no such pair by sorting the sequence S in O(n lg n); starting
+;;; from i = 0 and j = |S'|; returning if the sum S'i + S'j = x, or
+;;; recursing upon i + 1 if sum < x or j - 1 if sum > x.
+;;; 
+;;; i) is demonstrable from the code; ii) depends on the contradiction
+;;; that, had, say, i (without loss of generality) crossed some i'
+;;; such that S'i' + S'j' = x, Si + Sj should have been greater than
+;;; x. But j would have been decremented, not i incremented, resulting
+;;; in a contradiction.
