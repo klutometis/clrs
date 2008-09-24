a4af68390d2a7144d05e00766e4a2097f921c374
Peter Danenberg <pcd@wikitex.org>
2008-09-24 05:55:42 -0700

    real k-way merge

--- 6.5/6.5-8.scm
+++ 6.5/6.5-8.scm
@@ -8,3 +8,10 @@
                               '(3 4 5 10)
                               '(9 11 77 88))
        => '(1 2 3 4 5 7 8 9 10 11 77 88))
+
+;;; The above is merely O(n lg n).
+;;; 
+;;; For O(n lg k), initialize the heap with the first element of all k
+;;; lists; extract-min in O(lg k) and insert next element from the
+;;; list that min belonged to n times for O(n(lg(k) + lg(k))) = O(2 n
+;;; lg(k)) = O(n lg(k)).
