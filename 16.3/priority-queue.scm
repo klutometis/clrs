0a7aa627d42b2f86de5f50407fd33868b9c3101b
Peter Danenberg <pcd@wikitex.org>
2008-09-28 04:47:36 -0700

    priority queue with satellite data

---
+++ 16.3/priority-queue.scm
@@ -0,0 +1,108 @@
+(define (parent i)
+  (exact-floor (/ i 2)))
+
+(define (left i)
+  (+ (* i 2) 1))
+
+(define (right i)
+  (+ (* i 2) 1 1))
+
+(define-record-type :heap
+  (make-heap key set-key! more? extremum size data)
+  heap?
+  (key heap-key)
+  (set-key! heap-set-key!)
+  (more? heap-more?)
+  (extremum heap-extremum)
+  (size heap-size set-heap-size!)
+  (data heap-data set-heap-data!))
+
+(define (heap-length heap)
+  (length (heap-data heap)))
+
+(define (heap-swap! heap i j)
+  (swap! (heap-data heap) i j))
+
+(define (heap-ref heap i)
+  (list-ref (heap-data heap) i))
+
+(define (heap-set! heap i k)
+  (list-set! (heap-data heap) i k))
+
+(define (heapify! heap i)
+  (let ((l (left i))
+        (r (right i))
+        (key (heap-key heap))
+        (size (heap-size heap))
+        (more? (heap-more? heap)))
+    (let ((l-valid? (< l size))
+          (r-valid? (< r size)))
+      (let ((key-l (if l-valid? (key (heap-ref heap l))))
+            (key-r (if r-valid? (key (heap-ref heap r))))
+            (key-i (key (heap-ref heap i))))
+        (let ((extreme
+               (let* ((extreme-l
+                       (if (and l-valid?
+                                (more? key-l key-i))
+                           l
+                           i))
+                      (key-extreme-l (key (heap-ref heap extreme-l))))
+                 (if (and r-valid?
+                          (more? key-r key-extreme-l))
+                     r
+                     extreme-l))))
+          (if (not (= extreme i))
+              (begin
+                (heap-swap! heap i extreme)
+                (heapify! heap extreme))))))))
+
+(define (build-heap! heap)
+  (set-heap-size! heap (heap-length heap))
+  (let ((median (exact-floor (/ (heap-size heap) 2))))
+    (loop ((for i (down-from median (to 0))))
+          (heapify! heap i))))
+
+(define (heapsort! heap)
+  (let ((size (heap-size heap)))
+    (loop ((for i (down-from size (to 0))))
+          (heap-swap! heap 0 i)
+          (set-heap-size! heap (- size 1))
+          (heapify! heap i))))
+
+(define (heap-extract-extremum! heap)
+  (let ((size (heap-size heap)))
+    (if (< size 1)
+        (error "heap underflow -- EXTRACT-EXTREMUM"))
+    (let ((extremum (car (heap-data heap))))
+      (heap-set! 0 (heap-ref heap (- size 1)))
+      (set-heap-size! heap (- size 1))
+      (heapify! heap 0)
+      extremum)))
+
+(define (heap-adjust-key! heap i k)
+  (let ((key (heap-key heap))
+        (set-key! (heap-set-key! heap)))
+    (let* ((i-ref (heap-ref heap i))
+           (i-key (key i-ref)))
+      (if (and ((complement =) k i-key)
+               ((complement (heap-more? heap)) k i-key))
+          (error "new key violates heap gradient -- ADJUST-KEY!" k))
+      (set-key! i-ref k)
+      (loop continue ((with i i)
+                      (with parent-i (parent i) (parent i))
+                      (while (> i 0))
+                      ;; Strict inequality might reduce the complexity
+                      ;; by a constant
+                      (while ((complement (heap-more? heap))
+                              (key (heap-ref heap parent-i))
+                              (key (heap-ref heap i)))))
+            (heap-swap! heap i parent-i)
+            (continue (=> i parent-i))))))
+
+(define (heap-insert heap elt)
+  (let ((new-size (+ (heap-size heap) 1))
+        (key ((heap-key heap) elt)))
+    (set-heap-size! heap new-size)
+    (set-heap-data! heap (append (heap-data heap) (list elt)))
+    ((heap-set-key! heap) elt (heap-extremum heap))
+    (heap-adjust-key! heap (- new-size 1) key)))
---
+++ 16.3/section.scm
@@ -0,0 +1,15 @@
+(require-extension
+ syntax-case
+ foof-loop
+ (srfi 9))
+(require '../srfi/srfi-70)
+(require '../5.4/section)
+(module
+ section-16.3
+ (heapify!
+  make-heap
+  heap-data
+  build-heap!)
+ (import* srfi-70 exact-floor)
+ (import* section-5.4 swap! list-set!)
+ (include "../16.3/priority-queue.scm"))
