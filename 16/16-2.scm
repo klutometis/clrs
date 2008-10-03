bd8265d3de5d09da25fb7a204ed504c79c169ce2
Peter Danenberg <pcd@wikitex.org>
2008-10-02 23:34:30 -0700

    explain task strategies

--- 16/16-2.scm
+++ 16/16-2.scm
@@ -1,6 +1,9 @@
 (require-extension syntax-case check)
 (require '../16/chapter)
 (import chapter-16)
+;;; a. Minimizing the average is a matter of minimizing large numbers;
+;;; therefore, schedule smaller tasks first. O(n lg n) to heapify and
+;;; systematically extract-min.
 (let ((tasks (list
               (make-task 0 3 #f #f)
               (make-task 1 7 #f #f)
@@ -12,6 +15,13 @@
     (check (map task-id scheduled-tasks) => '(4 0 3 1 2))
     (check (average-completion scheduled-tasks) => (/ 62 5))))
 
+;;; b. The same principle of minimizing large numbers applise; when a
+;;; task is released, therefore, if its remaining time is less than
+;;; the remaining time of the current task, put the current task back
+;;; in the priority queue and preempt it with the lesser task.
+;;;
+;;; Otherwise, systematically extract the task with the least
+;;; remaining time from the priority queue.
 (let ((tasks (list
               (make-task 0 3 3 #f)
               (make-task 1 7 3 #f)
