92e14751acb1bf8eeb41fb6b1a804bdeb8b552c0
Peter Danenberg <pcd@roxygen.org>
2010-08-31 18:51:35 -0700

    rotation

--- 13/test-red-black-tree.scm
+++ 13/test-red-black-tree.scm
@@ -1,4 +1,4 @@
-(use defstruct debug)
+(use defstruct debug test)
 
 ;;; common-enough name; maybe importers can import it as
 ;;; red-black-node, if need be?
@@ -21,7 +21,8 @@
     (node-right-set! rotandum (node-left rotator))
     (if (not (node-null? (node-left rotator)))
         (node-parent-set! (node-left rotator) rotandum))
-    (node-parent-set! rotator rotandum)
+    (node-parent-set! rotator
+                      (node-parent rotandum))
     (if (node-null? (node-parent rotandum))
         (set! root rotator)
         (if (eq? rotandum (node-left (node-parent rotandum)))
@@ -36,7 +37,8 @@
     (node-left-set! rotandum (node-right rotator))
     (if (not (node-null? (node-right rotator)))
         (node-parent-set! (node-right rotator) rotandum))
-    (node-parent-set! rotator rotandum)
+    (node-parent-set! rotator
+                      (node-parent rotandum))
     (if (node-null? (node-parent rotandum))
         (set! root rotator)
         (if (eq? rotandum (node-right (node-parent rotandum)))
@@ -62,6 +64,9 @@
       (map (cut nested-map f <>) tree)
       (f tree)))
 
+(define (tree->pre-order-key-list root)
+  (nested-map node-key (tree->pre-order-list root)))
+
 (let ((node-38 (make-node color: 'black
                           key: 38
                           parent: nil))
@@ -80,7 +85,9 @@
       (node-39 (make-node color: 'black
                           key: 39
                           left: nil
-                          right: nil)))
+                          right: nil))
+      (pre-order-tree '(38 (21 #f #f) (41 (39 #f #f) (51 #f #f))))
+      (left-rotated-pre-order-tree '(41 (38 (21 #f #f) (39 #f #f)) (51 #f #f))))
   (node-left-set! node-38 node-21)
   (node-right-set! node-38 node-41)
   (node-left-set! node-41 node-39)
@@ -89,8 +96,17 @@
   (node-parent-set! node-41 node-38)
   (node-parent-set! node-39 node-41)
   (node-parent-set! node-51 node-41)
-  (debug (nested-map node-key (tree->pre-order-list node-38)))
+  (test
+   "tree structure"
+   pre-order-tree
+   (tree->pre-order-key-list node-38))
   (let ((left-rotated-root (left-rotate! node-38 node-38)))
-    (debug (nested-map node-key (tree->pre-order-list left-rotated-root)))
+    (test
+     "left rotation"
+     left-rotated-pre-order-tree
+     (tree->pre-order-key-list left-rotated-root))
     (let ((right-rotated-root (right-rotate! left-rotated-root left-rotated-root)))
-      (debug (nested-map node-key (tree->pre-order-list right-rotated-root))))))
+      (test
+       "right rotation inverse of left rotation"
+       pre-order-tree
+       (tree->pre-order-key-list right-rotated-root)))))
